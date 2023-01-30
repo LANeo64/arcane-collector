cd766e99c1674e27acbd1af2ebabf6407ae38ffc
Robert Čížek <laneo64@gmail.com>
2023-01-30 23:16:07 +0100

    Added polymorphic model support.

--- collector/collector/settings.py
+++ collector/collector/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'simple_history',
+    'polymorphic',
     'collection.apps.CollectionConfig',
     'inventory.apps.InventoryConfig',
 ]
