5032a4588a6956551bc2a4a46f5092afd5c1ba18
Robert Čížek <laneo64@gmail.com>
2023-02-11 16:14:39 +0100

    Changed the way of using views.

--- collector/collection/urls.py
+++ collector/collection/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from . import views
+from collection.views import ArchiveView
 
 app_name = 'collection'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', ArchiveView.as_view(), name='index'),
     path('detail/', TemplateView.as_view(template_name='collection/detail.html'), name='detail')
 ]
--- collector/collection/views.py
+++ collector/collection/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views.generic import TemplateView
+
+from collection.models import Collection
 
 # Create your views here.
-def index(request):
+class ArchiveView(TemplateView):
+    template_name = 'collection/archive.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['collections'] = Collection.objects.all()[:15]
+        return context
+
+
+def archive(request):
     template = loader.get_template('collection/collection.html')
     context = {
         'example': 'hello my collection'
